ee9fe38de08e661c4cf58ca94a573929d29b1702
comoestassergio <dirtycrew666@gmail.com>
2023-01-19 14:22:24 +0300

    Finish footer and fix type in header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import {AiFillGithub} from 'react-icons/ai'
 
 
 export default function Footer () {
     return (
-        <footer className="flex flex-col gap-3 h-[20vh]">
-            <div>
-                <p>Created by <Link href={'https://github.com/comoestassergio'} target='_blank'>comoestassergio</Link></p>
-                <p><Link href={'https://github.com/D3vd/Meme_Api'} target='_blank'>API</Link> by <Link href={'https://github.com/D3vd'} target='_blank'>D3vd</Link></p>
+        <footer className="flex flex-col gap-3 h-[20vh] bg-gray-900 pt-10">
+            <div className="w-[90%] h-px bg-gray-600 mx-auto md:w-[75%]"></div>
+            <div className="text-gray-500 w-[90%] mx-auto md:w-[75%]">
+                <p className="text-lg">Created by <AiFillGithub className=" inline-block text-2xl ml-1" /> <Link className="font-medium hover:underline" href={'https://github.com/comoestassergio'} target='_blank'>comoestassergio</Link></p>
+                <p><Link className="font-medium hover:underline" href={'https://github.com/D3vd/Meme_Api'} target='_blank'>API</Link> by <Link className="font-medium hover:underline" href={'https://github.com/D3vd'} target='_blank'>D3vd</Link></p>
+                <p><Link className="font-medium hover:underline" href={'https://en.wikipedia.org/wiki/Meme'} target='_blank'>Memes</Link> from <Link className="font-medium hover:underline" href={'https://www.reddit.com/'} target='_blank'>Reddit</Link></p>
             </div>
         </footer>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header () {
         <header className="flex items-center justify-between p-5 bg-gray-100">
             <div className="w-[90%] mx-auto md:w-[75%] flex flex-col items-start gap-1">
                 <h1 className='text-3xl font-medium text-gray-800'>Memerr.</h1>
-                <span className="opacity-70 text-sm translate-y-1">That's where all the memes at</span>
+                <span className="opacity-70 text-sm translate-y-1">That&apos;s where all the memes at</span>
             </div>
         </header>
     )
